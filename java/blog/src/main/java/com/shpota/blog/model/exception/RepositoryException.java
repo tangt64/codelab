009aa9211f8b84340a938d041da8631bcb68056e
Choi Gook Hyun <bluehelix@gmail.com>
2024-08-18 22:52:37 +0900

    NFS
    망할 NFS...

--- java/blog/src/main/java/com/shpota/blog/model/exception/RepositoryException.java
+++
@@ -1,7 +0,0 @@
-package com.shpota.blog.model.exception;
-
-public class RepositoryException extends RuntimeException {
-    public RepositoryException(Throwable cause) {
-        super(cause);
-    }
-}
