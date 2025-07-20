eecff8d4fca218a04cf283533d954a6b455d432f
Choi Gook Hyun <bluehelix@gmail.com>
2025-07-20 23:13:33 +0900

    저장소 추가
    자잘한 버그 수정 및 저장소 추가

--- bootcamp/IaC-bootcamp/k8s-Pulumi/Pulumi.ts
+++ bootcamp/IaC-bootcamp/k8s-Pulumi/Pulumi.ts
@@ -1,9 +1,31 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as command from "@pulumi/command";
 
-const masterIp = "192.168.1.10";
-const workerIp = "192.168.1.11";
-const sshPrivateKey = "/home/user/.ssh/id_rsa";
+const masterIp = "10.0.2.225";
+const workerIp = "10.0.3.19";
+const sshPrivateKey = "/root/.ssh/id_rsa";
+
+// 저장소 등록
+const addYumRepo = new command.remote.Command("add-yum-repo", {
+    connection: {
+        host: masterIp,
+        port: 22,
+        user: "root",
+        privateKey: require("fs").readFileSync(sshPrivateKey).toString(),
+    },
+    create: `cat <<EOF> /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
+EOF
+
+dnf makecache
+dnf install kubeadm kubectl kubelet -y
+`,
+});
 
 // 마스터 초기화
 const initMaster = new command.remote.Command("init-master", {
@@ -14,7 +36,7 @@ const initMaster = new command.remote.Command("init-master", {
         privateKey: require("fs").readFileSync(sshPrivateKey).toString(),
     },
     create: `kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${masterIp}`,
-});
+}, { dependsOn: [addYumRepo] });
 
 // 마스터 노드의 join 명령어 추출
 const getJoinCmd = new command.remote.Command("get-join-command", {
@@ -35,7 +57,7 @@ const joinWorker = new command.remote.Command("join-worker", {
         user: "root",
         privateKey: require("fs").readFileSync(sshPrivateKey).toString(),
     },
-    create: getJoinCmd.stdout.apply(joinCmd => `${joinCmd} --v=5`),
+    create: getJoinCmd.stdout.apply((joinCmd: string) => `${joinCmd} --v=5`),
 }, { dependsOn: [getJoinCmd] });
 
 export const kubeadmJoinCommand = getJoinCmd.stdout;
